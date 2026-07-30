cd323af1bd0a40a69461277a8f88aa299efa34a5
benn0r <benjamin@benn0r.me>
2026-07-30 21:55:52 +0200

    test: collect unit coverage in one process

---
+++ tests/unit/index.test.ts
@@ -0,0 +1,7 @@
+import './api-response.test';
+import './crop.test';
+import './image-data.test';
+import './image-reference.test';
+import './image-storage.test';
+import './library.test';
+import './storage.test';
